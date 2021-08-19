ed09600eb2845085895470d284e18b30defb6740
a774500050 <qq774500050@gmail.com>
2021-08-19 17:17:12 +0800

    Fix trophy error, musicroom text, proof ed 1. Remove extra coma in e06, e07, e08 2. Fix trophy #27 error 3. Fix musicroom #9 4. update create_allmsg script

--- thtk/create_allmsg.ps1
+++ thtk/create_allmsg.ps1
@@ -2,24 +2,26 @@
 
 # create msg from txt
 Get-ChildItem ..\dialogue_cn\ |
-Where-Object {$_.Name -match '^st.*gb18030' } |
-ForEach-Object{
+Where-Object { $_.Name -match '^st.*gb18030' } |
+ForEach-Object {
     $parttern = 'st\d{2}\w'
     $match = select-string -Pattern $parttern -inputobject $_.BaseName
     $Base = $match.matches.groups[0].value
-    $OutName =  $Base + '.msg'
-   .\thmsg.exe -c 18 $_.FullName ..\data\$OutName
+    $OutName = $Base + '.msg'
+    #    .\thmsg.exe -c 18 $_.FullName ..\data\$OutName
+    $(.\thmsg.exe -c 18 $($_.FullName) ..\data\$($OutName))
 }
 
 # create ending from txt
 
 Get-ChildItem ..\dialogue_cn\ |
-Where-Object {$_.Name -match '^e.*gb18030' } |
-ForEach-Object{
+Where-Object { $_.Name -match '^e.*gb18030' } |
+ForEach-Object {
     $parttern = '^e\d{2}'
     $match = select-string -Pattern $parttern -inputobject $_.BaseName
     $Base = $match.matches.groups[0].value
-    $OutName =  $Base + '.msg'
-   .\thmsg.exe -e -c 18 $_.FullName ..\data\$OutName
+    $OutName = $Base + '.msg'
+    #    .\thmsg.exe -e -c 18 $_.FullName ..\data\$OutName
+    $(.\thmsg.exe -e -c 18 $($_.FullName) ..\data\$($OutName))
 }
 
